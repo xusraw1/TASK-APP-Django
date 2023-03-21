7356622fe6cfe8ba8348269c8f5b8b827d4f0b38
Xusrav <xusrav200329@gmail.com>
2023-03-21 18:24:24 +0500

    I modifided models.py in blog app, it makes it possible to view the name of the title in the admin panel

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,6 @@ class Task(models.Model):
     """Create Task Table in BD"""
     title = models.CharField('Task-name', max_length=50)
     text = models.TextField()
+
+    def __str__(self):
+        return self.title
