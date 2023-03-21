ad44f214fc7f2a00aad2f5c154edf0147658da32
Xusrav <xusrav200329@gmail.com>
2023-03-21 18:16:53 +0500

    Create model in BD, and make migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Task(models.Model):
+    """Create Task Table in BD"""
+    title = models.CharField('Task-name', max_length=50)
+    text = models.TextField()
